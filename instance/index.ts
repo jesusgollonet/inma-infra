6b26fe278f7eb20ca19c523bae35341b11e0a285
Jesús Gollonet <jesusgollonet@gmail.com>
2024-05-31 08:11:21 +0200

    Install docker compose on the machine. closse #2 (#3)

--- instance/index.ts
+++ instance/index.ts
@@ -3,7 +3,7 @@ import * as hcloud from "@pulumi/hcloud";
 // Create a new Hetzner Cloud Server from a snapshot
 const server = new hcloud.Server("my-server", {
   serverType: "cx11",
-  image: "167679213", // TODO: parameterize
+  image: "167702948", // TODO: parameterize
   location: "nbg1",
   sshKeys: ["inma-infra"], // TODO: parameterize
 });
